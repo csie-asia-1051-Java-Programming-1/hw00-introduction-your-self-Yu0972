29a19e39b25fbca97d9e3832cae23a6064818457
Yu0972 <105021045@live.asia.edu.tw>
2016-09-12 15:48:00 +0800

    Hw00-1 submit

---
+++ Hw00-1/Hw.java
@@ -0,0 +1,15 @@
+
+public class Hw {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		system.out.println("105021045");
+		System.out.println("³¢·¶§e");
+		
+	}
+
+}
